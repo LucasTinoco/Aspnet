3a3d3b95ed6ee5c366cdc1f0c07e59ae9f841c8d
LucasTinoco <59653669+LucasTinoco@users.noreply.github.com>
2020-01-17 13:10:30 -0300

    Site_Pessoal

---
+++ C#/Aspnet/Laboratios/SitePessoal/SitePessoal/Controllers/ContatoController.cs
@@ -0,0 +1,60 @@
+﻿using SitePessoal.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Web.Mvc;
+
+namespace SitePessoal.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Concluido = false;
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Salvar(Contato contato)
+        {
+            try
+            {
+                if(string.IsNullOrEmpty(contato.Mail)
+                       || string.IsNullOrEmpty(contato.Name)
+                       || string.IsNullOrEmpty(contato.Message))
+                {
+                    throw new ValidationException("Favor preencher todo formulário");
+                }
+
+                var pathVitual = "~/contatos.txt";
+                var pathFisico = Server.MapPath(pathVitual);
+
+                using (var sw = new StreamWriter(pathFisico, true))
+                {
+                    sw.Write(contato.Name+";");
+                    sw.Write(contato.Mail + ";");
+                    sw.Write(contato.Message + ";");
+                    sw.Write(DateTime.Now + ";\n");
+            
+                }
+
+
+                    ViewBag.Concluido = true;
+            }
+            catch(ValidationException ex)
+            {
+                ViewBag.ErrorMsg = ex.Message;
+                ViewBag.Concluido = false;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                ViewBag.ErrorMsg = "Ocorreu erro ao salvar arquivo, entre em contato com o administrador do sistema";
+                ViewBag.Concluido = false;
+            }
+
+            return View("Index");
+        }
+    }
+}
---
+++ C#/Aspnet/TesteMVC/TesteMVC/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TesteMVC.Models;
+
+namespace TesteMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var p = new Produto()
+            {
+                ProdutoId = 1,
+                Preco = 10,
+                Nome = "Produto 1"
+            };
+
+            return View(p);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Formulario()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Salvar(Usuario usuario)
+        {
+            return View("Formulario");
+        }
+    }
+}
